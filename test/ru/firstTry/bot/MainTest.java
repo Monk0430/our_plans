262bc02f02cfe94316c32848f7a6dcb1179a4bb0
stepan <baleevskikhsv@yandex.ru>
2021-09-23 10:01:33 +0500

    Stepan Погода + тесты

--- test/ru/firstTry/bot/MainTest.java
+++ test/ru/firstTry/bot/MainTest.java
@@ -4,35 +4,13 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
 
+import java.io.IOException;
+
 class MainTest {
     @Test
     void testCorrectHelloMessage() {
         String name = "User";
         String helloMessage = Main.getHelloMessage(name);
         assertEquals("Hello, " + name + "!", helloMessage);
-    }
-    @Test
-    void testCorrectCommandOutput() {
-        Processing command = new Processing();
-        String comm = command.processing("-anecdote");
-        assertEquals("Anecdote", comm);
-    }
-    @Test
-    void test2() {
-        Processing command = new Processing();
-        String comm = command.processing("-help");
-        assertEquals("-weather - показывает погоду в Екатеринбурге.\n-anecdote - показывает рандомный анекдот.", comm);
-    }
-    @Test
-    void test3() {
-        Processing command = new Processing();
-        String comm = command.processing("-weather");
-        assertEquals("14", comm);
-    }
-    @Test
-    void test4() {
-        Processing command = new Processing();
-        String comm = command.processing("-someOtherUnknownCommand");
-        assertEquals("Неизвестная комманда.", comm);
     }
 }
--- test/ru/firstTry/bot/ProcessingTest.java
+++ test/ru/firstTry/bot/ProcessingTest.java
@@ -4,27 +4,29 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
 
+import java.io.IOException;
+
 class ProcessingTest {
     @Test
-    void testCorrectCommandOutput() {
+    void testCorrectCommandOutput() throws IOException {
         Processing command = new Processing();
         String comm = command.processing("-anecdote");
-        assertEquals("Anecdote", comm);
+        assertEquals("Колобок повесился.", comm);
     }
     @Test
-    void test2() {
+    void test2() throws IOException {
         Processing command = new Processing();
         String comm = command.processing("-help");
         assertEquals("-weather - показывает погоду в Екатеринбурге.\n-anecdote - показывает рандомный анекдот.", comm);
     }
+//    @Test
+//    void test3() throws IOException {
+//        Processing command = new Processing();
+//        String comm = command.processing("-weather");
+//        assertEquals("14", comm);
+//    }
     @Test
-    void test3() {
-        Processing command = new Processing();
-        String comm = command.processing("-weather");
-        assertEquals("14", comm);
-    }
-    @Test
-    void test4() {
+    void test4() throws IOException {
         Processing command = new Processing();
         String comm = command.processing("-someOtherUnknownCommand");
         assertEquals("Неизвестная комманда.", comm);
