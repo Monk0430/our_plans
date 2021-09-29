0852d5537dcfe19211d9256aa41bd6e7b2aebfb0
Monk0430 <car0430@mail.ru>
2021-09-29 21:23:24 +0500

    Перенес "погоду" в отдельный файлик

---
+++ src/ru/firstTry/bot/Weather.java
@@ -0,0 +1,37 @@
+package ru.firstTry.bot;
+
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.net.URLConnection;
+
+public class Weather {
+    public static int get_weather() throws IOException {
+        String url = "https://api.openweathermap.org/data/2.5/weather?" +
+                "q=Ekaterinburg" +
+                "&utils=metric" +
+                "&appid=4fec795d30e972f1082d9c81736071ed";
+        String data = get(url);
+        JSONObject obj = new JSONObject(data);
+        return (int) (obj.getJSONObject("main").getDouble("temp") - 273.15);
+
+    }
+
+    private static String get(String link) throws IOException {
+        URL url = new URL(link);
+        URLConnection yc = url.openConnection();
+        BufferedReader in = new BufferedReader(
+                new InputStreamReader(yc.getInputStream())
+        );
+        String inputLine;
+        String result = "";
+
+        while ((inputLine = in.readLine()) != null)
+            result = inputLine;
+        in.close();
+        return result;
+    }
+}
